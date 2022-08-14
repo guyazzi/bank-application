5b6e525bcc3a28b3c3c3b94011e9aae725764acf
gazzi <guy-azzi@hotmail.com>
2022-08-13 23:21:04 -0400

    adding @CrossOrigin at Controllers level

--- src/main/java/com/bankapplication/controller/CustomerController.java
+++ src/main/java/com/bankapplication/controller/CustomerController.java
@@ -6,14 +6,12 @@ import com.bankapplication.service.customer.ICustomerService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @AllArgsConstructor
+@CrossOrigin
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
